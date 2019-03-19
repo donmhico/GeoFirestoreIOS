7c6bf7aedd067736aafefb63d4418cd279136575
Mico Panaga <mico@theevoeon.com>
2019-03-19 14:07:17 +0800

    Initial Commit

---
+++ GeoFirestoreIOS/GeoFirestoreIOS.h
@@ -0,0 +1,19 @@
+//
+//  GeoFirestoreIOS.h
+//  GeoFirestoreIOS
+//
+//  Created by Mico on 19/03/2019.
+//  Copyright © 2019 HELPEE-TECH INC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GeoFirestoreIOS.
+FOUNDATION_EXPORT double GeoFirestoreIOSVersionNumber;
+
+//! Project version string for GeoFirestoreIOS.
+FOUNDATION_EXPORT const unsigned char GeoFirestoreIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeoFirestoreIOS/PublicHeader.h>
+
+
